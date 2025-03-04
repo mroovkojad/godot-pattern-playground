c1f0a4fe93800fb4849bb0dc14c9934b6e9d3f39
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-03-04 21:17:35 +0100

    Comment out unfinised classes

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,51 +1,51 @@
 
 public partial class Game : Node
 {
-    private Node _currentScene = null;
-    private PackedScene _mainMenuScene;
-    private PackedScene _levelScene;
+    // private Node _currentScene = null;
+    // private PackedScene _mainMenuScene;
+    // private PackedScene _levelScene;
+//
+    // public override void _Ready()
+    // {
+    //     // Load scene resources
+    //     _mainMenuScene = GD.Load<PackedScene>("res://MainMenu.tscn");
+    //     _levelScene = GD.Load<PackedScene>("res://Level.tscn");
 
-    public override void _Ready()
-    {
-        // Load scene resources
-        _mainMenuScene = GD.Load<PackedScene>("res://MainMenu.tscn");
-        _levelScene = GD.Load<PackedScene>("res://Level.tscn");
+    //     // Start with the main menu
+    //     ChangeSceneTo(_mainMenuScene);
+    // }
 
-        // Start with the main menu
-        ChangeSceneTo(_mainMenuScene);
-    }
+    // private void ChangeSceneTo(PackedScene sceneResource)
+    // {
+    //     // Remove current scene if it exists
+    //     if (_currentScene != null)
+    //     {
+    //         RemoveChild(_currentScene);
+    //         _currentScene.QueueFree();
+    //     }
 
-    private void ChangeSceneTo(PackedScene sceneResource)
-    {
-        // Remove current scene if it exists
-        if (_currentScene != null)
-        {
-            RemoveChild(_currentScene);
-            _currentScene.QueueFree();
-        }
+    //     // Instance the new scene
+    //     _currentScene = sceneResource.Instantiate();
+    //     AddChild(_currentScene);
 
-        // Instance the new scene
-        _currentScene = sceneResource.Instantiate();
-        AddChild(_currentScene);
+    //     // Connect signals if they exist
+    //     if (_currentScene is MainMenu mainMenu)
+    //     {
+    //         mainMenu.StartGame += OnStartGame;
+    //     }
+    //     else if (_currentScene is Level level)
+    //     {
+    //         level.ReturnToMenu += OnReturnToMenu;
+    //     }
+    // }
 
-        // Connect signals if they exist
-        if (_currentScene is MainMenu mainMenu)
-        {
-            mainMenu.StartGame += OnStartGame;
-        }
-        else if (_currentScene is Level level)
-        {
-            level.ReturnToMenu += OnReturnToMenu;
-        }
-    }
+    // private void OnStartGame()
+    // {
+    //     ChangeSceneTo(_levelScene);
+    // }
 
-    private void OnStartGame()
-    {
-        ChangeSceneTo(_levelScene);
-    }
-
-    private void OnReturnToMenu()
-    {
-        ChangeSceneTo(_mainMenuScene);
-    }
+    // private void OnReturnToMenu()
+    // {
+    //     ChangeSceneTo(_mainMenuScene);
+    // }
 }
--- scripts/menus/MainMenu.cs
+++ scripts/menus/MainMenu.cs
@@ -1,23 +1,22 @@
 
 using System.Diagnostics.Tracing;
 
-public partial class MainMenu : Control
+public partial class MainMenu //: Control
 {
-    [Signal]
-    public delegate void StartGameEventHandler();
-
-    private Button _startButton;
-
-    public override void _Ready()
-    {
-        // Get reference to the start button
-        _startButton = GetNode<Button>("StartButton");
-        _startButton.Pressed += OnStartButtonPressed;
-    }
-
-    private void OnStartButtonPressed()
-    {
-        EmitSignal(SignalName.StartGame);
-    }
+   // [Signal]
+   // public delegate void StartGameEventHandler();
+//
+   // private Button _startButton;
+//
+   // public override void _Ready()
+   // {
+   //     _startButton = GetNode<Button>("StartButton");
+   //     _startButton.Pressed += OnStartButtonPressed;
+   // }
+//
+   // private void OnStartButtonPressed()
+   // {
+   //     EmitSignal(SignalName.StartGame);
+   // }
 
 }
